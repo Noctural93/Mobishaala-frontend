5688c9350fe3d7fdcd5c6d94100b0685b5f139d6
Noctural93 <gowtham313754@gmail.com>
2024-04-18 23:34:44 +0530

    first commit

---
+++ client/src/components/ImgCard.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const ImgCard = ({details}) => {
+  return (
+    <div className='gallary-items'>
+      <img src={details.value.mapValue.fields.txtVal.stringValue}/>
+      <p>{details.value.mapValue.fields.geoLocation.mapValue.fields.city.stringValue}, {details.value.mapValue.fields.geoLocation.mapValue.fields.regionName.stringValue
+}, {details.value.mapValue.fields.geoLocation.mapValue.fields.countryCode.stringValue}, {
+    details.value.mapValue.fields.geoLocation.mapValue.fields.zip.stringValue
+}</p>
+    </div>
+  )
+}
+
+export default ImgCard
